95731e15b6ae98c40360b5a0995a85d57e12a094
w1zm8 <vv1zm8@gmail.com>
2020-07-26 03:00:10 +0300

    implemented csv data parsing in CsvReader class

--- src/CsvReader.ts
+++ src/CsvReader.ts
@@ -1,12 +1,32 @@
 import { readFileSync } from "fs";
+import { transformStrToDate } from "./utils";
+
+type Row = [Date, string, string, string, number];
 
 export class CsvReader {
-  data: string[][] = [];
+  data: Row[] = [];
 
   constructor(public path: string) {}
 
-  private parseCsvString(data: string): string[][] {
-    return data.split("\n").map((row: string) => row.split(","));
+  private parseCsvString(data: string): Row[] {
+    return data
+      .split("\n")
+      .map((row: string) => row.split(","))
+      .map(
+        (row: string[]): Row => {
+          return [
+            this.parseDate(row[0]),
+            row[1],
+            row[2],
+            row[3],
+            parseInt(row[4]),
+          ];
+        }
+      );
+  }
+
+  parseDate(dateString: string): Date {
+    return transformStrToDate(dateString);
   }
 
   read(): void {
--- src/__tests__/CsvReader.test.ts
+++ src/__tests__/CsvReader.test.ts
@@ -8,9 +8,23 @@ describe("CsvReader", () => {
     expect(csvReader.data).toHaveLength(9);
     expect(csvReader.data[0]).toHaveLength(5);
 
+    // date
+    expect(csvReader.data[0][0].toDateString()).toBe("Mon Jun 01 2020");
+    // racetrack
+    expect(csvReader.data[0][1]).toBe("Davisstad");
+    // driver
+    expect(csvReader.data[0][2]).toBe("Rico Kihn");
+    // team
+    expect(csvReader.data[0][3]).toBe("Ab sequi dolorum.");
+    // points
+    expect(csvReader.data[0][4]).toEqual(expect.any(Number));
+    expect(csvReader.data[0][4]).toBe(72);
+
     //console.log(csvReader.data);
   });
   it("method `parseDate` returns parsed date", () => {
-    expect(csvReader.parseDate("1/6/2020")).toEqual("");
+    expect(csvReader.parseDate("1/6/2020").toDateString()).toBe(
+      "Mon Jun 01 2020"
+    );
   });
 });
